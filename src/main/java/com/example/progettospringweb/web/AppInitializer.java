c808881c51041f8d2bd35145fb9d2c6911e5d7e6
GiovaCode89 <g.giovanni100@gmail.com>
2023-02-21 12:05:13 +0100

    prima_commit

---
+++ src/main/java/com/example/progettospringweb/web/AppInitializer.java
@@ -0,0 +1,28 @@
+package com.example.progettospringweb.web;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        return super.createDispatcherServlet(servletAppContext);
+    }
+}
